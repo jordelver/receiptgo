03e49cc2da090a2ef4b12b22ea5856ef2d375d00
Jordan Elver <jordan@elver.me>
2023-02-10 17:54:25 +0000

    Extract last of logic from `main.rs` into function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,10 @@
 use clap::Parser;
 
 use receiptgo::cli;
-use receiptgo::ringgo::{authentication, downloads, parking_sessions};
-
-/// Number of receipts to download
-static RECEIPTS_TO_DOWNLOAD: usize = 5;
 
 #[tokio::main]
 async fn main() {
     let args = cli::Args::parse();
 
-    let access_token = authentication::get_token(args.username, args.password, args.client_secret)
-        .await
-        .unwrap();
-
-    let parking_sessions = parking_sessions::retrieve_parking_sessions(&access_token)
-        .await
-        .unwrap();
-
-    if let Some(ps) = parking_sessions {
-        for session in ps.sessions.into_iter().take(RECEIPTS_TO_DOWNLOAD) {
-            println!("Downloading {}", session.id);
-
-            let download_result = downloads::download_receipt_pdf(&access_token, session.id).await;
-
-            if download_result.is_ok() {
-                println!("> Downloaded");
-            } else {
-                println!("> Download failed");
-            }
-
-            println!();
-        }
-    } else {
-        println!("No parking sessions");
-    }
+    receiptgo::run(args).await
 }
